ffa2da0e4eeceacbab51ef3fd989f34045a926f8
JunNishi <e095745@ie.u-ryukyu.ac.jp>
2019-10-08 14:40:29 +0900

    @変数postsコントローラーに書き直し

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,10 @@
 class PostsController < ApplicationController
   def index
+    @posts = [
+      "今日からProgateでRailsの勉強するよー!!!",
+      "投稿一覧ページ作成中!!",
+      "旅費がでないよ〜。",
+      "ひろみGOOL!!"
+    ]
   end
 end
