5f806a9e02f4c3d302aaf2a5bc4b126b6c48aac6
JunNishi <e095745@ie.u-ryukyu.ac.jp>
2019-10-09 07:58:13 +0900

    postsインスタンスを作成し，each文で表示

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,5 @@
 class PostsController < ApplicationController
   def index
-    @posts = [
-      "今日からProgateでRailsの勉強するよー!!!",
-      "投稿一覧ページ作成中!!",
-      "旅費がでないよ〜。",
-      "ひろみGOOL!!"
-    ]
+    @posts = Post.all
   end
 end
