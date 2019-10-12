41ec539c15266fc4fe0a50b3ff7ffb4486619210
JunNishi <e095745@ie.u-ryukyu.ac.jp>
2019-10-13 01:47:32 +0900

    ユーザをデータベースに保存する

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,18 @@ class UsersController < ApplicationController
   def show
     @user = User.find_by(id: params[:id])
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(name: params[:name], email: params[:email])
+
+    if @user.save
+      redirect_to("/users/index")
+    else
+      render("/users/new")
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,19 @@
 Rails.application.routes.draw do
-  get 'posts/index' => 'posts#index'
-  get 'posts/new' => 'posts#new'
-  post 'posts/create' => 'posts#create'
-  get 'posts/:id/edit' => 'posts#edit'
-  post 'posts/:id/update' => 'posts#update'
-  post 'posts/:id/destroy' => 'posts#destroy'
+  get '/posts/index' => 'posts#index'
+  get '/posts/new' => 'posts#new'
+  post '/posts/create' => 'posts#create'
+  get '/posts/:id/edit' => 'posts#edit'
+  post '/posts/:id/update' => 'posts#update'
+  post '/posts/:id/destroy' => 'posts#destroy'
 
-  get 'posts/:id' => "posts#show"
+  get '/posts/:id' => "posts#show"
   
   get({'/' => 'home#top'})
   
-  get 'about' => 'home#about' 
+  get '/about' => 'home#about' 
 
-  get 'users/index' => 'users#index'
-  get 'users/:id' => 'users#show'
+  get '/users/index' => 'users#index'
+  get '/signup' => 'users#new'
+  post '/users/create' => 'users#create'
+  get '/users/:id' => 'users#show'
 end
