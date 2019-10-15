2d4b4783d758ac5dc99ec01e58fdde83c8d5843c
nishi <e095745@ie.u-ryukyu.ac.jp>
2019-10-15 18:55:39 +0900

    rails db:migrateでimage_nameカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_11_142209) do
+ActiveRecord::Schema.define(version: 2019_10_15_093653) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2019_10_11_142209) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_name"
   end
 
 end
