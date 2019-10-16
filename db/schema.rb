8f1b349583c290496fb3057bb3dc05b3486e4ad0
nishi <e095745@ie.u-ryukyu.ac.jp>
2019-10-16 09:07:16 +0900

    postsテーブルにuser_idカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_15_133242) do
+ActiveRecord::Schema.define(version: 2019_10_15_235946) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
