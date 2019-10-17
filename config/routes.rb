d1a867ef52c02cf8eb6437bef08eab68c21bfb86
nishi <e095745@ie.u-ryukyu.ac.jp>
2019-10-17 13:24:18 +0900

    いいねしている投稿一覧を表示する

--- config/routes.rb
+++ config/routes.rb
@@ -25,4 +25,5 @@ Rails.application.routes.draw do
 
   post '/likes/:post_id/create' => 'likes#create'
   post '/likes/:post_id/destroy' => 'likes#destroy'
+  get '/users/:id/likes' => 'users#likes'
 end
